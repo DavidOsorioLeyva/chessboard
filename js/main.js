3a42946b9fe9d5fb37abc2069eeacfc54b8c42d3
David Osorio <davidosorioleyva@outlook.com>
2021-07-31 23:08:58 -0500

    Starting with the board animation

--- js/main.js
+++ js/main.js
@@ -1,46 +1,19 @@
-let startButton = document.getElementById('start')
-let pawnOne = document.getElementById('pawn-one')
-let pawnTwo = document.getElementById('pawn-two')
-let bishopOne = document.getElementById('bishop-one')
-let horseOne = document.getElementById('horse-one')
-let queenOne = document.getElementById('queen-one')
-let horseTwo = document.getElementById('horse-two')
-let pawnThree = document.getElementById('pawn-three')
-let space1 = document.querySelector('.horse__space')
-let space2 = document.querySelector('.space__horse__two')
+const pawnOne = document.getElementById('e2')
+const pawnTwo = document.getElementById('e7')
+const bishop = document.getElementById('Bf1')
+const knightOne = document.getElementById('Nb8')
+const queen = document.getElementById('Qd1')
+const knightTwo = document.getElementById('Ng8')
+const pawnThree = document.getElementById('f7')
 
-const animation1 = () => pawnOne.style.transform = 'translateY(-200%)'
+const mainBtn = document.querySelector('.main__btn')
 
-const animation2 = () => pawnTwo.style.transform = 'translateY(200%)'
-
-const animation3 = () => bishopOne.style.transform = 'translate(-300%, -300%)'
-
-const animation4 = () => {
-    horseOne.style.transform = 'translate(100%, 200%)'
-    space1.classList.add('wht-c')
-}
-
-const animation5 = () => queenOne.style.transform = 'translate(400%, -400%)'
-
-const animation6 = () => {
-    horseTwo.style.transform = 'translate(-100%, 200%)'
-    space2.classList.remove('wht-c')
-    space2.classList.add('blck-c')
-}
-
-const animation7 = () => {
-    queenOne.style.transform = 'translate(200%, -600%)'
-    pawnThree.style.display = 'none'
-}
+const animation1 = () => pawnOne.style.animationName = 'move-one'
+const animation2 = () => pawnTwo.style.animationName = 'move-two'
 
 const startAnimation = () => {
-    setTimeout(animation1, 500)
-    setTimeout(animation2, 1500)
-    setTimeout(animation3, 2500)
-    setTimeout(animation4, 3500)
-    setTimeout(animation5, 4500)
-    setTimeout(animation6, 5500)
-    setTimeout(animation7, 6500)
+    animation1()
+    animation2()
 }
 
-startButton.addEventListener('click', startAnimation)
+mainBtn.addEventListener('click', startAnimation)
