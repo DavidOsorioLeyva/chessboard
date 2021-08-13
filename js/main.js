c7369b60eea4a306e7b1a68843bc431b429391ad
David Osorio <davidosorioleyva@outlook.com>
2021-08-12 22:55:57 -0500

    Modified indentation to 2 spaces

--- js/main.js
+++ js/main.js
@@ -9,16 +9,16 @@ const pawnThree = document.getElementById('f7')
 const mainBtn = document.querySelector('.main__btn')
 
 const startAnimation = () => {
-    setTimeout(() => pawnOne.style.animationName = 'move-one', 200)
-    setTimeout(() => pawnTwo.style.animationName = 'move-two', 1000)
-    setTimeout(() => bishop.style.animationName = 'move-three', 1800)
-    setTimeout(() => knightOne.style.animationName = 'move-four', 2600)
-    setTimeout(() => queen.style.animationName = 'move-five', 3400)
-    setTimeout(() => knightTwo.style.animationName = 'move-six', 4200)
-    setTimeout(() => {
-        queen.style.animationName = 'move-seven'
-        pawnThree.style.animationName = 'hidden'
-    }, 5000)
+  setTimeout(() => pawnOne.style.animationName = 'move-one', 200)
+  setTimeout(() => pawnTwo.style.animationName = 'move-two', 1000)
+  setTimeout(() => bishop.style.animationName = 'move-three', 1800)
+  setTimeout(() => knightOne.style.animationName = 'move-four', 2600)
+  setTimeout(() => queen.style.animationName = 'move-five', 3400)
+  setTimeout(() => knightTwo.style.animationName = 'move-six', 4200)
+  setTimeout(() => {
+    queen.style.animationName = 'move-seven'
+    pawnThree.style.animationName = 'hidden'
+  }, 5000)
 }
 
 mainBtn.addEventListener('click', startAnimation)
